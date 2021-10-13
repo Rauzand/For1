2613a5130322cb55fbcba6e3aa303f218e00945f
Rittey Иттер <ritey@list.ru>
2021-10-13 14:35:19 +0800

    Вставил класс логики из 1 лабораторной работы по for

--- WinForm_For/Form1.cs
+++ WinForm_For/Form1.cs
@@ -16,7 +16,18 @@ namespace WinForm_For
         {
             InitializeComponent();
         }
-
+        public class Logic
+        {
+            public static int Res(int x, int y)
+            {
+                int proiz = 0;
+                for (int i = 1; i <= y; i++)
+                {
+                    proiz += x;
+                }
+                return proiz;
+            }
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
